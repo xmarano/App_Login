080094d954cbc6bf84f829e3a91b4ac606cfda28
xmarano <leo.gregori@epitech.eu>
2024-06-05 15:16:52 +0200

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Léo Grégori on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
